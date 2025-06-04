1a9d0718823753fdab0a71b98ef8762a4f9b2255
agriyantosk <agriyantosk@gmail.com>
2025-06-04 20:36:50 +0700

    Feat: Update User Data Routes and Controller

--- backend-repo/controller/userControllers.ts
+++ backend-repo/controller/userControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { fetchUserData } from "../repository/userCollection";
+import { fetchUserData, updateUserData } from "../repository/userCollection";
 import { sendSuccess, throwError } from "../utils/responsesHandler";
 
 const userControllers = {
@@ -10,6 +10,26 @@ const userControllers = {
       const userData = await fetchUserData(user.uid);
 
       sendSuccess(res, userData, "Successfully fetched user!", 200);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  updateUserData: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = req.user;
+      if (!user) throwError("User not found!", 404);
+
+      const { totalAverageWeightRatings, numberOfRents, recentlyActive } =
+        req.body;
+      const updatedData = {
+        totalAverageWeightRatings,
+        numberOfRents,
+        recentlyActive,
+      };
+
+      const updateUser = await updateUserData(user.uid, updatedData);
+      sendSuccess(res, updateUser, "Successfully update user data!", 200);
     } catch (error) {
       next(error);
     }
--- backend-repo/routes/userRoutes.ts
+++ backend-repo/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import userControllers from "../controller/userControllers";
 const userRoutes = express.Router();
 
 userRoutes.get("/", userControllers.fetchUserData);
+userRoutes.patch("/", userControllers.updateUserData);
 
 export { userRoutes };

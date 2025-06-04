29332275e22b3ccf7fd7b005c6d08a3ae1bdb10f
agriyantosk <agriyantosk@gmail.com>
2025-06-04 20:39:02 +0700

    Chore: Implement Global Error Handler

--- backend-repo/core/app.ts
+++ backend-repo/core/app.ts
@@ -4,12 +4,15 @@ dotenv.config();
 import express from "express";
 import { userRoutes } from "../routes";
 import authMiddleware from "../middleware/authMiddleware";
+import errorHandler from "../middleware/errorHandler";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(authMiddleware);
 app.use("/api/user", userRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

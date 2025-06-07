34e6e50f8c513d667ac4edf585e6d9d3e84aae83
agriyantosk <agriyantosk@gmail.com>
2025-06-07 17:32:28 +0700

    Chore: Install and Configure Firebase

--- frontend-repo/src/lib/firebaseAuth.ts
+++ frontend-repo/src/lib/firebaseAuth.ts
@@ -1,12 +1,28 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { signOut } from "firebase/auth";
 import { auth } from "./firebaseClient";
+// import Cookies from "js-cookie";
 
 export const login = async (email: string, password: string) => {
-  const userCredential = await signInWithEmailAndPassword(
-    auth,
-    email,
-    password
-  );
-  const token = await userCredential.user.getIdToken();
-  return { user: userCredential.user, token };
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const token = await userCredential.user.getIdToken();
+    return { user: userCredential.user, token };
+  } catch (error) {
+    console.error("Full Firebase Error:", error);
+  }
+};
+
+export const logout = async () => {
+  try {
+    await signOut(auth);
+    // Cookies.remove("firebaseToken");
+  } catch (error) {
+    console.error("Logout failed:", error);
+    throw error;
+  }
 };
